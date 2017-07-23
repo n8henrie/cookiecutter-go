510bf80300ac4fecea95f8d40bde7b9422d833da
Nathan Henrie <nate@n8henrie.com>
2017-07-23 15:11:49 -0600

    Add version printing

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.go
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
+
+var version = "undefined"
+
+func printVersion() {
+	fmt.Println("{{ cookiecutter.repo_name }} version:", version)
+	os.Exit(0)
+}
 
 // Documentation for main function
 func main() {
+	showVersion := flag.Bool("version", false, "Print version")
 
+	flag.Parse()
+
+	if *showVersion {
+		printVersion()
+	}
 }
